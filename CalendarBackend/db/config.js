32d8fc68a94a4ef3a79b7e7a81a50ae40541734e
Lillian Shieh <lillian.shieh95@gmail.com>
2018-06-19 13:36:22 -0400

    h

--- CalendarBackend/db/config.js
+++ CalendarBackend/db/config.js
@@ -10,9 +10,11 @@ let db
 
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     db = pgp({
-    database: process.env.PG_DATABASE || 'gamblr',
-    port: process.env.PG_PORT || 5432,
-    host: process.env.PG_HOST || 'localhost',
+        database: process.env.PG_DATABASE || 'gamblr',
+        port: process.env.PG_PORT || 5432,
+        host: process.env.PG_HOST || 'localhost',
+        username: 'lillian',
+        password: '123'
     });
 } else if (process.env.NODE_ENV === 'production') {
     db = pgp(process.env.DATABASE_URL);
