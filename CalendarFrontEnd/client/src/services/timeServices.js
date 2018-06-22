1990140783ccdcc3c4887c04457515857540b976
Lillian Shieh <lillian.shieh95@gmail.com>
2018-06-21 23:35:53 -0400

    shown time bugs

--- CalendarFrontEnd/client/src/services/timeServices.js
+++ CalendarFrontEnd/client/src/services/timeServices.js
@@ -3,10 +3,15 @@ export default {
 	convert24to12(time) {
 		let sections = time.split(':')
 		let result = sections.slice(0,2)
+        result[0] = parseInt(result[0])
 		let ending = 'AM'
-		if(parseInt(result[0]) > 12) {
-			result[0] = (parseInt(result[0])-12).toString()
+		if(result[0] > 12) {
+			result[0] = result[0]-12
 			ending = 'PM'
+		} else if(result[0] === 12) {
+			ending = 'PM'
+		} else if(result[0] === 0) {
+			result[0] = 12
 		}
 		return `${result[0]}:${result[1]}${ending}`
 	}
